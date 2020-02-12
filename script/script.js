76ffbc2490c5c2d8ac0def5a563333ab3e6b3d15
Lenny <theiign@gmail.com>
2020-02-12 16:07:35 +0100

    Create book submition form

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 // DOM nodes
 const createBookBtn = document.querySelector(".create-book-btn");
+const bookAuthorInput = document.querySelector(".book-author-input");
 const bookTitleInput = document.querySelector(".book-title-input");
+const bookPageNumInput = document.querySelector(".book-pagenum-input");
+const isReadInput = document.querySelector("#is-read");
 
 // Event listeners
 createBookBtn.addEventListener('click', addBookToLibrary);
@@ -10,11 +13,14 @@ let myLibrary = [];
 function Book(author, title, pageNum, isRead) {
     this.author = author;
     this.title = title;
+    this.pageNum = pageNum;
+    this.isRead = isRead;
 }
 
 function addBookToLibrary() {
-    // do stuff here
+    let author = bookAuthorInput.value;
     let title = bookTitleInput.value;
-    console.log(title);
-    myLibrary.push(new Book("placeholder", title));
+    let pagenum = bookPageNumInput.value;
+    let isRead = isReadInput.checked;
+    myLibrary.push(new Book(author, title, pagenum, isRead));
 }
