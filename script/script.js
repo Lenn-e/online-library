c737a621f403a066db3d4a1ec697f944030ef181
Lenny <theiign@gmail.com>
2020-02-12 17:33:58 +0100

    Create book display function

--- script/script.js
+++ script/script.js
@@ -4,11 +4,16 @@ const bookAuthorInput = document.querySelector(".book-author-input");
 const bookTitleInput = document.querySelector(".book-title-input");
 const bookPageNumInput = document.querySelector(".book-pagenum-input");
 const isReadInput = document.querySelector("#is-read");
+const bookDisplay = document.querySelector(".book-display");
 
 // Event listeners
 createBookBtn.addEventListener('click', addBookToLibrary);
 
 let myLibrary = [];
+
+myLibrary.push(new Book('qwer', 'asdf', 'zxcv', true));
+myLibrary.push(new Book('ayy', 'lmao', 'buddy', true));
+myLibrary.push(new Book('idk', 'random', 'text', false));
 
 function Book(author, title, pageNum, isRead) {
     this.author = author;
@@ -24,3 +29,42 @@ function addBookToLibrary() {
     let isRead = isReadInput.checked;
     myLibrary.push(new Book(author, title, pagenum, isRead));
 }
+
+function bookToNode(book) {
+    let outterContainer = document.createElement('div');
+    outterContainer.classList.add('book-item');
+
+    let titleContainer = document.createElement('div');
+    titleContainer.classList.add('title-container');
+
+    let titleDisplay = document.createElement('h2');
+    titleDisplay.textContent = book.title;
+
+    let authorDisplay = document.createElement('h3');
+    authorDisplay.textContent = book.author;
+
+    let pageNumDisplay = document.createElement('div');
+    pageNumDisplay.textContent = book.pageNum;
+
+    let isReadDisplay = document.createElement('div');
+    if(book.isRead) {
+        isReadDisplay.classList.add('read');
+    } else {
+        isReadDisplay.classList.add('unread');
+    }
+
+    outterContainer.appendChild(titleContainer);
+    outterContainer.appendChild(pageNumDisplay);
+    outterContainer.appendChild(isReadDisplay);
+    titleContainer.appendChild(titleDisplay);
+    titleContainer.appendChild(authorDisplay);
+
+    return outterContainer;
+}
+
+function displayBooks() {
+    myLibrary.forEach(book => {
+        bookDisplay.appendChild(bookToNode(book));
+    })
+}
+
